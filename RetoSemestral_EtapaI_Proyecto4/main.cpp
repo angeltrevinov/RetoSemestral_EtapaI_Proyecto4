d19b06a92b9196b99afc045a2be72e4f7b8a263d
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2017-10-12 10:52:09 -0500

    Initial Commit

---
+++ RetoSemestral_EtapaI_Proyecto4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RetoSemestral_EtapaI_Proyecto4
+//
+//  Created by Angel Trevino on 10/12/17.
+//  Copyright © 2017 Angel Odiel Treviño Villanueva. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
