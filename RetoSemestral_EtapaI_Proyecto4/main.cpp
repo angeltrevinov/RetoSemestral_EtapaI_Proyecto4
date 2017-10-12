2df4ccb8551773b6db15d4e9ef7004e7284c91ea
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2017-10-12 10:55:25 -0500

    Agregar Instrucciones

--- RetoSemestral_EtapaI_Proyecto4/main.cpp
+++ RetoSemestral_EtapaI_Proyecto4/main.cpp
@@ -1,15 +1,38 @@
 //
-//  main.cpp
-//  RetoSemestral_EtapaI_Proyecto4
-//
-//  Created by Angel Trevino on 10/12/17.
-//  Copyright © 2017 Angel Odiel Treviño Villanueva. All rights reserved.
-//
+//  ListasEncadenadas (Clase)
+//  Angel Odiel Treviño Villanueva
+//  A01336559
+//  Estructura de Datos
 
 #include <iostream>
+#include "Lista.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    Lista <int> L;
+    L.insertaInicio(5);
+    L.insertaInicio(2);
+    L.insertaInicio(8);
+    L.insertaFinal(3);
+    L.muestra();
+    cout << "El tamaño es " << L.regresaTamanio() << endl;
+    /*L.borraInicio();
+     L.borraFinal();
+     L.muestra();*/
+    Lista <int> copy(L);
+    copy.muestra();
+    copy.reverse();
+    copy.Shift(-10);
+    copy.borraFinal();
+    if(copy.igual(L))
+        cout << "son iguales" <<endl;
+    else
+        cout << "son diferentes" <<endl;
+    copy.muestra();
+    copy.operator+=(L);
+    L.muestra();
+    copy.muestra();
+    //copy.deleteLista(3, 3); no me funciono
+    
     return 0;
 }
